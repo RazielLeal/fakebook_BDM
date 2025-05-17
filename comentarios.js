1051a7f6c8c74531f995c700d4e4d48185a3b821
Lumiel13 <100006168+Lumiel13@users.noreply.github.com>
2025-05-17 15:52:10 -0600

    Delete comentarios.js

--- comentarios.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.comentarioInput').forEach(textarea => {
-    textarea.addEventListener('keydown', function(e) {
-        if (e.key === 'Enter' && !e.shiftKey) {
-            e.preventDefault(); // Prevenir salto de línea
-            const form = this.closest('form');
-            form.submit(); // Enviar el formulario
-        }
-    });
-});
